1e96c7a1764a27c4b140b39df9b0709aebd01b7a
Arnav K <arnavkaushal09@gmail.com>
2024-10-31 16:38:31 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,8 @@ import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), solidJs()]
+  integrations: [tailwind(), solidJs()],
+  site: "https://arnavk-09.github.io",
+  base: "imggen",
+  output: "static"
 });
